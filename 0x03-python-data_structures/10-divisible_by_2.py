0677baadea4e4bfe57021e7f63f3708e723584a9
Eman-Bakri <eman1996-sy@hotmail.com>
2023-09-09 11:11:17 -0700

    dividable by 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    boolean = my_list[:]
+    for counter, i in enumerate(my_list):
+        if i % 2 == 0:
+            boolean[counter] = True
+        else:
+            boolean[counter] = False
+    return(boolean)
