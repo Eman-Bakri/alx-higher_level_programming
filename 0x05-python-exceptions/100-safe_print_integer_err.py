df264dd0567a7abeb35b1b17173e922026aadabd
Eman-Bakri <eman1996-sy@hotmail.com>
2023-09-25 19:56:10 -0700

    int error 3

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,5 +7,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except ValueError as error:
-        print("Exception: {}".format(error), file=sys.stderr)
+        sys.stderr.write("Exception: {}\n".format(error))
         return False
