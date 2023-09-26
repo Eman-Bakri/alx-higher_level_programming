7c21cf2fa1066bbf18f9a127a6c895df9c9ada99
Eman-Bakri <eman1996-sy@hotmail.com>
2023-09-26 15:49:21 -0700

    square with size attr

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
+"""Definition of Square class."""
 
 
 class Square:
-
+    """Square Representation."""
+	 
     def __init__(self, size):
-        self.__size = size
+      """New Square initialized.
+      
+      Args:
+        size(int): New square size.
+      """
+
+      self.__size = size
