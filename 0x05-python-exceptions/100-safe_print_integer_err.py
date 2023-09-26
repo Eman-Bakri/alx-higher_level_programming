6b6d66d8eedc968ff6dc64dc8066efaeec1aa194
Eman-Bakri <eman1996-sy@hotmail.com>
2023-09-25 19:02:08 -0700

    print int error

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 import sys
 
+
 def safe_print_integer_err(value):
+    int_bool = True
     try:
         print("{:d}".format(value))
     except (TypeError, ValueError):
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return False
-    else:
-        return True
+        int_bool =  False
 
