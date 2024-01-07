06879eb728ccc8b93222fa2a5456b14e5b034d74
Eman-Bakri <eman1996-sy@hotmail.com>
2024-01-07 15:53:58 -0800

    status response

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Module to fetche a specific url
+"""
+import urllib.request
+
+
+def main():
+    """
+    Funtion to print a status response
+    """
+    url = 'https://intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode('utf8')))
+
+if __name__ == "__main__":
+    main()
+
