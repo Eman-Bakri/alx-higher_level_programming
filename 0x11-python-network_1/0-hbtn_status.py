9ff46d341f9fbdbde4552beffa588c45a79ca60e
Eman-Bakri <eman1996-sy@hotmail.com>
2024-01-07 16:06:00 -0800

    status response

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,22 +1,12 @@
 #!/usr/bin/python3
-"""
-Module to fetche a specific url
-"""
-import urllib.request
-
-
-def main():
-    """
-    Funtion to print a status response
-    """
-    url = 'https://intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as response:
-        html = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode('utf8')))
-
+"""module to  fetch a specific url"""
 
 if __name__ == "__main__":
-    main()
+    import urllib.request
+    requrl = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(requrl) as response:
+        response_content = response.read()
+        print("Body response:")
+        print(f"\t- type: {type(response_content)}")
+        print(f"\t- content: {response_content}")
+        print(f"\t- utf8 content: {response_content.decode()}")
